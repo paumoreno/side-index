accf5a49a9ab2c6069b9fe95d66cb96c7ce990f8
Pau Moreno <pau@series.ly>
2015-06-28 23:48:20 +0200

    Explain code

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# side-index
-Easily navigate through a long list of items divided in categories.
+jQuery Side Index
+=================
+
+A jQuery plugin that builds a side index similar to the one you can find in your smartphone contact list.
+
+How to use it
+-------------
+
+The minimum requirement to build a side index is to have a list of items (no specific markup is required) and a series of anchor targets in it. For example, given an alphabetic list of names, each  initial letter could be an anchor target.
+
+These anchor targets must have an `id` attribute. In order to create the side index, just add the boolean attribute `data-side-index` to each of the anchor targets.
+
+```
+    <ul id="names">
+        <li id="A" data-side-index>
+            A
+        <li>
+        <li>
+            Alice
+        </li>
+        ...
+        <li id="B" data-side-index>
+            B
+        <li>
+        <li>
+            Bob
+        </li>
+        ...
+    </ul>
+```
+
+Then, create the side index like this:
+
+```
+$('#names').sideIndex();
+```
+
+**Note:** There should be only one indexed list per page.
+
+Additional markup
+-----------------
+
+The contents of the side index can be modified by adding some more information in the list anchor targets.
+
+By default, the section labels in the side index are taken from the value of the `id` attributes of each anchor target. You can use different labels by setting the value of the `data-side-index` attributes:
+
+`<li id="h12" data-side-index="12">`
+
+When swiping through the side index, an indicator appears showing the section currently shown. By default, the label of this indicator is the `id` attribute of the anchor target, or the value of its `data-side-index` attribute. If you need to give it a differnt label, just add the `data-side-index-indicator` attribute with the value you wish.
+
+`<li id="h12" data-side-index="12" side-index-indicator="12:00">`
+
+Options
+-------
+
+These are the optional parameters you can pass to jQuery Side Index:
+
+- `indexTop`: Top margin of the side index, expressed in pixels (default is 0). Useful to avoid overlapping the side index with a fixed top bar.
+
+- `targetTopOffset`: Top margin that will be left when scrolling to the anchor targets, expressed in pixels (default is 0). Useful to avoid a fixed top bar to hide the anchor title.
+
+```
+$('#names').sideIndex({
+    indexTop: 50,
+    targetTopOffset: 60
+});
+```
+
+Author
+------
+
+Pau Moreno Martín
+
+[@paumoreno](https://twitter.com/paumoreno)
--- js/jquery.sideIndex.js
+++ js/jquery.sideIndex.js
@@ -1,33 +1,55 @@
-(function($) {
+(function($, window) {
     'use strict';
 
     $.fn.sideIndex = function(opts) {
         var $context = this,
-            sideIndexElements = [],// Array containing a reference to all the elements in the side index.
+
+            debug = true,
+
+            // Array containing a reference to all the elements in the side index.
+            sideIndexElements = [],
+
             currentRef = null,
-            lastMatchedIndex = 0,// A reference to the last matched element index in the side index. This prevents scrolling to the last element accidentally.
+
+            // Structure of the side index
             $sideIndexTemplate = $('<nav />', {'id': 'side-index'}).append($('<ul />')),
+
+            // Structure of each side index element
             $indexElementTemplate = $('<li />', {'class': 'side-index-item'}).append($('<a />')),
+
+            // Structure of the selected index item indicator
             $positionIndicatorTemplate = $('<div />', {'id': 'side-index-indicator'}),
+
+            // Plugin default values for optional parameters
             defaults = {
-                indexTop: '0',
+
+                // Top margin in pixels for the side index. Use it to prevent overlapping with a fixed topbar
+                indexTop: 100,
+
+                // Height pixel offset to apply when positioning the viewport to any of the selected positions
+                // Useful when there is a fixed topbar that can overlap the upper part of the selected section
                 targetTopOffset: 0
             },
             options;
 
         /**
-         * Gets the data of all the indexable elements of the page and returns it in an array.
+         * Gets the information of all the indexable elements of the page @asyncnd returns it in an array.
          */
-        var buildNavArray = function() {
-            var navElements = [];
+        var getNavElements = function() {
+            var navElements = [],
+                $inexableElements;
 
             // Get the data of all the indexable elements in the document.
             $('[data-side-index]', $context).each(function() {
+                var indicatorText = $(this).data('sideIndexIndicator'),
+                    indexText = $(this).data('sideIndex'),
+                    id = $(this).attr('id');
+
                 navElements.push({
                     pos: $(this).offset().top,
-                    text: $(this).data('sideIndex'),
-                    indicator: $(this).data('sideIndexIndicator') || $(this).data('sideIndex'),
-                    ref: $(this).attr('id')
+                    text: indexText || id,
+                    indicator: indicatorText || indexText || id,
+                    ref: id
                 });
             });
 
@@ -37,16 +59,13 @@
                 return a.pos - b.pos;
             });
 
-
-            //console.log("side elements:");
-            //
-            //$.each(navElements, function() {
-            //    console.log(this.pos, this.ref);
-            //});
-
             return navElements;
         };
 
+        /**
+         * Build an array that contains a references to the elements in the side index.
+         * It will be used to detect the location of the finger when swiping over the side index.
+         */
         var buildIndexArray = function() {
             var docOffset = $(window).scrollTop();
 
@@ -57,30 +76,35 @@
                 });
             });
 
-            //console.log("index elements:");
-            //
-            //$.each(sideIndexElements, function() {
-            //    console.log(this.pos, this.obj.data('target'));
-            //});
+            if (debug) {
+                console.log("Side index elements:");
+                $.each(sideIndexElements, function() {
+                   console.log(this.pos, this.obj.data('target'));
+                });
+            }
         };
 
+        /**
+         * Build the side index DOM, appending an item for each nav element
+         */
         var buildSideIndex = function() {
             var $sideIndex = $sideIndexTemplate.clone(),
                 $indexElement,
-                i,
                 windowHeight = $(window).height(),
-                navElements = buildNavArray();
+                navElements = getNavElements(),
+                i;
 
             for (i = 0; i < navElements.length; i++) {
                 $indexElement = $indexElementTemplate
                     .clone()
-                    .data('target', navElements[i].ref)
+                    .data('target',     navElements[i].ref)
                     .data('indicator', navElements[i].indicator)
                     .text(navElements[i].text);
 
                 $('ul', $sideIndex).append($indexElement);
             }
 
+            // Set the size of the side index according to the given options
             $sideIndex.css('height', (windowHeight + 30) + 'px');
             $('ul', $sideIndex).css('height', (windowHeight - options.indexTop) + 'px');
 
@@ -89,6 +113,9 @@
             buildIndexArray();
         };
 
+        /**
+         * Show a marker that indicates which section is the user scrolling to
+         */
         var placeMarker = function(text) {
             var $indicator = $positionIndicatorTemplate.clone();
 
@@ -100,24 +127,30 @@
 
         /**
          * Bring the viewport to the position specified by $sideIndexElement
-         * @param $sideIndexElement Element of the side Index that stores the reference to the element to reveal
-         * @param showMarker boolean Should I show a marker indicating the current position??
+         *
+         * @param $sideIndexElement Element of the side index that stores the reference to the element to scroll to
+         * @param showMarker boolean Should I show a marker indicating the current position?
          */
         var goToRef = function($sideIndexElement, showMarker) {
             var target;
 
+            // Prevent scrolling to the same section
             if (currentRef != $sideIndexElement.data('target')) {
                 currentRef = $sideIndexElement.data('target');
 
-                //console.log($sideIndexElement.data('target'));
-                //window.location.hash = $sideIndexElement.data('target');
+                if (debug) {
+                    console.log("Moving the viewport to ", $sideIndexElement.data('target'));
+                }
 
+                // Set the URL search part to the corresponding indexed section id.
                 if (window.history.replaceState) {
+                    // If possible, replace the history state to prevent multiple "backs" to go to the previous page
                     window.history.replaceState(window.history.state, document.title, '#' + $sideIndexElement.data('target'));
                 } else {
                     window.location.hash = $sideIndexElement.data('target');
                 }
 
+                // Scroll to the appropriate section
                 target = $('#' + $sideIndexElement.data('target'));
                 window.scrollTo(0, target.offset().top + options.targetTopOffset);
 
@@ -130,36 +163,38 @@
         var bindEvents = function() {
             $context
                 .on('touchstart', '#side-index', function(e) {
-
+                    // Prevent scrolling the document if the swipe starts in the side index
                     e.preventDefault();
                 })
                 .on('touchmove', '#side-index', function(e) {
                     var y = e.originalEvent.touches[0].clientY,
-                        i,
-                        foundPos = false;
-
-                    //console.log(y, y  - docOffset);
+                        i;
 
                     for (i = 0; i < sideIndexElements.length; i++) {
                         if (y < sideIndexElements[i].pos) {
-                            lastMatchedIndex = i;
-                            foundPos = true;
                             break;
                         }
                     }
 
-                    if (!foundPos) {
-                        i = lastMatchedIndex;
+                    // If the finger is avobe the first index element, go to the first element
+                    if (i === 0) {
+                        i = 1;
+                    }
+
+                    if (debug) {
+                        console.log("Finger at Y ", y);
+                        console.log("Over position " + i, sideIndexElements[i - 1].obj.data('target'));
                     }
 
                     goToRef(sideIndexElements[i - 1].obj, true);
 
                     e.preventDefault();
                 })
-                .on('touchend', '#side-index', function(e) {
+                .on('touchend', '#side-index', function() {
                     $context.find('#side-index-indicator').fadeOut('fast');
                 })
                 .on('click', '.side-index-item', function() {
+                    // If a side index element is tapped, just go to the corresponding section
                     goToRef($(this), false);
                 });
         };
@@ -170,4 +205,4 @@
         bindEvents();
     };
 
-}(jQuery));
+}(jQuery, window));
